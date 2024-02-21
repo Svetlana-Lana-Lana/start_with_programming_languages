f0b29b1dbfb03bea3fe02bdbe6f5c942f39e8923
Svetlana <Svetlanat1005@yandex.ru>
2024-02-21 12:15:15 +0300

    add Lesson2:Task2

---
+++ Lesson2/Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Создать массив и заполнить его целыми числами от 1 до N (N - протзвольное натуральное число).
+// вывести элементы массива на экран.
+
+int n = 10;
+int[] arr = new int[n];
+int i = 0;
+
+while(i < n){
+//while(i < arr.Length){
+    arr[i] = i + 1;
+    //Console.Write(arr[i]);
+    //Console.Write(arr[' ']);
+    Console.Write($"{arr[i]} ");
+     i = i + 1;
+}
+
