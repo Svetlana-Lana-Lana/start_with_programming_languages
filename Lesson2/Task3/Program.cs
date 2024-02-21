feade01fe2a1f6c34b633bea892994a3d99aac6a
Svetlana <Svetlanat1005@yandex.ru>
2024-02-21 12:38:55 +0300

    add Lesson2:Task3

---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Создать массив на 10 произвольных целых чисел. Вывести на экран четные элементы массива.
+
+//int n = 10;
+int[] array = {1, 2, 3, 5, 6, 7, 8, 9, 6, 3};
+int i = 0;
+//while(i < n)
+while(i < array.Length)
+{
+    if(array[i] % 2 == 0)
+    {
+//        Console.Write($"{array[i]} ");
+        Console.Write(array[i] + " ");
+    }
+    i = i+1;
+}
