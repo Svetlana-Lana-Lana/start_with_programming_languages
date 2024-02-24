90a3a956cb5833aaca82b87b6ad2e7d7b71a7387
Svetlana <Svetlanat1005@yandex.ru>
2024-02-24 13:57:26 +0300

    add lesson3:task1

---
+++ Lesson3/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denomenator = c + d;
+    double result = numenator / denomenator;
+    return result;
+}
+double result = CalculateFormula(1, 2, 3, 4);
+Console.WriteLine(result);
+
+
+
+
+
+
+
+
+
+//int n = 5;
+
+//int i = 1;
+//while(i <= n){
+//    Console.Write($"{i*i} ");
+//    i = i + 1;
+//}
