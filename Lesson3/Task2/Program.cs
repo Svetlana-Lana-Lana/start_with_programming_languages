171dc9143106c6c5bac60468807265de0effb73f
Svetlana <Svetlanat1005@yandex.ru>
2024-02-24 16:56:07 +0300

    add lesson3:task2

---
+++ Lesson3/Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿//int n = 5;
+
+//int i = 1;
+//while(i <= n){
+//    Console.Write($"{i*i} ");
+//    i = i + 1;
+//}
+
+//int n = 10;
+
+//int i = 1;
+//while(i <= n){
+//    Console.Write($"{i*i} ");
+//    i = i + 1;
+//}
+
+//int n = 15;
+
+//int i = 1;
+//while(i <= n){
+//    Console.Write($"{i*i} ");
+//    i = i + 1;
+//}
+
+void PrintSquares(int limit)
+{
+    int i = 1;
+    while (i <= limit)
+    {
+        Console.Write($"{i * i} ");
+        i++;
+    }
+}
+PrintSquares(5);
+PrintSquares(10);
+PrintSquares(15);
