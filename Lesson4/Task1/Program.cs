dad712a4cf3289a60b490b9309ea2dc2a46f0c4a
Svetlana <Svetlanat1005@yandex.ru>
2024-02-28 14:53:15 +0300

    add Task1

---
+++ Lesson4/Task1/Program.cs
@@ -0,0 +1,53 @@
+﻿int[,] matrix = new int[3, 5];
+
+Random rnd = new Random();
+for (int i = 0; i < matrix.GetLength(0); i++)//4-11 заполнение массива
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+
+        matrix[i, j] = rnd.Next(1, 11);
+    }
+}
+
+for (int i = 0; i < matrix.GetLength(0); i++)//13-20 вывод массива на экран
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+
+        Console.Write($"{matrix[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+int[,] CreateMatrix(int rowCount, int columsCount);
+{
+    int[,] matrix = new int[rowCount, columsCount];
+
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)//4-11 заполнение массива
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix;
+}
+
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)//13-20 вывод массива на экран
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+
+        Console.Write($"{matrix[i, j]} ");
+    }
+    Console.WriteLine();
+}
+}
+int[,] matrix = CreateMatrix(4, 5);
+ShowMatrix(matrix);
